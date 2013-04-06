459f2f4c925b63f4d2ce31dd07926f51f9a0275c
carl sednaoui <carlsednaoui@gmail.com>
2013-04-06 13:34:50 -0400

    refactor problem 3 answer

--- problem_3.rb
+++ problem_3.rb
@@ -1,21 +1,40 @@
 number = 600851475143
-@chain = []
 
-def is_prime?(n)
-  (2..n-1).each do |i|
-    return n % i == 0 ? false : true
-  end
-end
+# Version 1
+# @chain = []
 
-def get_primes(n)
+# def is_prime?(n)
+#   (2..n-1).each do |i|
+#     return n % i == 0 ? false : true
+#   end
+# end
+
+# def get_primes(n)
+#   (2..n).each do |i|
+#     if is_prime?(i) && (n % i == 0)
+#       @chain.push(i)
+#       get_primes(n / i)
+#       break
+#     end
+#   end
+# end
+
+# get_primes(number)
+# puts @chain.last
+
+# Version 2
+def get_prime(n)
   (2..n).each do |i|
-    if is_prime?(i) && (n % i == 0)
-      @chain.push(i)
-      get_primes(n / i)
-      break
+    if i == n
+      return n
+    elsif n % i == 0
+      return get_prime(n/i)
     end
   end
 end
 
-get_primes(number)
-puts @chain.last
+puts get_prime(number)
+
+# Version 3
+# require 'prime'
+# puts number.prime_division.inspect
