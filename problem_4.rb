c24da133ef3680bbc7fd0c9006fb76e497633ba7
carl sednaoui <carlsednaoui@gmail.com>
2013-04-07 12:26:59 -0400

    update problem 4 solution

--- problem_4.rb
+++ problem_4.rb
@@ -2,8 +2,8 @@ start_time = Time.now
 
 palindromes = []
 
-(1..999).to_a.reverse.each do |y|
-  (1..999).to_a.reverse.each do |z|
+(1..999).each do |y|
+  (1..999).each do |z|
     if (y * z).to_s == (y * z).to_s.reverse
       palindromes.push(y * z)
     end
